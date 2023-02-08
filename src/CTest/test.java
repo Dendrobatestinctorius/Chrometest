a3c474f072dc4915be0b969ab70c088c29ea93a0
Dendrobates Studio Adam Machowski <dendrobates.studio@gmail.com>
2023-02-08 10:26:21 +0100

    SE resault

--- src/CTest/test.java
+++ src/CTest/test.java
@@ -8,22 +8,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.*;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium lib\\chromedriver.exe");
         WebDriver drv = new ChromeDriver();
-
         String burl = "http://Google.com";
         drv.get(burl);
+
         drv.manage().deleteCookieNamed ("CONSENT");
         drv.manage().addCookie(new Cookie("CONSENT","YES+shp.gws-"+LocalDate.now().toString().replace("-","")+"-0-RC2.en+FX+374"));
         drv.navigate().refresh();
         WebElement querry = drv.findElement(By.name("q"));
         querry.sendKeys("Hello World");
         querry.submit();
-        //WebDriverWait cwait = new WebDriverWait(drv, Duration.ofSeconds(10L) );
-        //cwait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("consent")));
-        //drv.quit();
+        Duration tmo = Duration.ofSeconds(5);
+        WebDriverWait seWait = new WebDriverWait(drv, tmo );
+        seWait.until(ExpectedConditions.presenceOfElementLocated(By.id("rso")));
+        List<WebElement> elements = drv.findElement(By.id("rso")).findElements(By.xpath("//div[@class='g']"));
+
+        for(WebElement element: elements){
+            System.out.println(element.getText());
+        }
+
+        drv.quit();
     }
 }
